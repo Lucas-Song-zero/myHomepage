975766ecafdb9a08b0c2b3091ff50c6b5f6b5bbf
Weitao Jiang <wjiang0415@outlook.com>
2026-01-18 19:52:36 +0800

    test CD

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+HOST="www.weitao-jiang.cn"
+WORKDIR="/srv/homepage/Home"
+SERVICENAME="homepage"
+
+
+if [ "$(id -u)" = "0" ]; then
+    echo "Switching to user runner"
+    su - runner -c "$0"
+    exit
+fi
+# git clone
+
+cd $WORKDIR
+git pull origin master
+cd $WORKDIR/..
+echo "Build python dependency"
+
+python3 -m venv venv
+source venv/bin/activate
+
+pip install -r $WORKDIR/requirements.txt -q
+pip install gunicorn -q
+
+echo "Restarting service $SERVICENAME"
+sudo systemctl restart $SERVICENAME
+
+echo "Deployment completed, visit https://$HOST"
