db326c6e0a60e0289efd0cd9b4dac28c68b6849a
Song XZ <songxz23@mails.tsinghua.edu.cn>
2026-01-20 16:01:10 +0800

    first add to this repo

--- app.py
+++ app.py
@@ -181,5 +181,5 @@ from api_booking import booking_bp
 app.register_blueprint(booking_bp)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 443))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
